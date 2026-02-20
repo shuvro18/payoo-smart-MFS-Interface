c04a33ed3776671191ea7dee2b70b7674e253040
shuvro18 <shuvrobiswas404@gmail.com>
2026-02-20 19:12:14 +0600

    login page done

---
+++ script/login.js
@@ -0,0 +1,22 @@
+document.getElementById("login-button").addEventListener("click", function () {
+    // input numbeR
+    const inputNumber = document.getElementById("login-number");
+    const number = inputNumber.value ;
+
+    // input pin
+    const inputPin = document.getElementById("login-pin");
+    const pin = inputPin.value ;
+    
+    // condition
+    
+    if(number==="12345678900" && pin=== "1234"){
+        alert("Login Successful");
+
+        window.location.assign("../home.html")
+    }
+    else{
+        alert("Login Failed😓");
+        return;
+    }
+    
+});
